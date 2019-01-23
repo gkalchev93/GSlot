7c2bde48a57018b5e62a8b7727862be635063564
Georgi Kalchev <g.kalchev@devnyacement.bg>
2019-01-23 11:25:26 +0200

    Implemented interfaces for the Client side - IPresenter and IUserInteraction

---
+++ GSlot.Logic/Interfaces/IPresenter.cs
@@ -0,0 +1,13 @@
+﻿using GSlot.Model;
+using System.Collections.Generic;
+
+namespace GSlot.Logic.Interfaces
+{
+    public interface IPresenter
+    {
+        void ShowMessage(string msg);
+        void ShowGrid(List<char[]> grid);
+        void ShowPlayerStats(Player player, decimal lastSpinWon);
+        void Clear();
+    }
+}
---
+++ GSlot.Logic/Interfaces/IUserInteraction.cs
@@ -0,0 +1,9 @@
+﻿namespace GSlot.Logic.Interfaces
+{
+    public interface IUserInteraction
+    {
+        string GetUserInput();
+        PlayerCommand GetPlayerCommand();
+        void Exit();
+    }
+}
